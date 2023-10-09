7285d8aa0df64c4e776734191da86a846e7a4103
Astrid <github@astrid.exposed>
2023-10-10 01:14:19 +0200

    user-agent

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ module.exports = {
 	websocket: {
 		url: "wss://api.rblxroll.com/socket.io/?EIO=4&transport=websocket",
 		headers: {
-			"user-agent": `rblxroll-rain-socket/${require("../package.json")?.version || "?"} (github.com/robertsspaceindustries/rblxroll-rain-socket)`,
+			"user-agent": `rblxroll-rain-socket/${require("../package.json")?.version || "?"}`,
 		},
 	},
 };
